8e35b3b9da70264b7f1ea0fb9f41d0f47db6c157
Eshaan Sharma <eshaansharma800@gmail.com>
2026-02-20 00:52:50 +0530

    fixed railway port + env

--- server.js
+++ server.js
@@ -53,6 +53,9 @@ app.delete("/card/:id", async (req, res) => {
 });
 
 // Server Start
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
+
